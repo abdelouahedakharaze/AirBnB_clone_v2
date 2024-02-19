8ead140c5710906ef9d18eea4000809d6cfdd8f9
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-20 00:53:15 +0100

    task7_try2

--- models/user.py
+++ models/user.py
@@ -2,10 +2,10 @@
 """
 This module defines a class User
 """
-from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
@@ -14,14 +14,17 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = "users"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
+        places = relationship(
+            "Place", backref="user", cascade="all, delete, delete-orphan"
+        )
+        reviews = relationship(
+            "Review", backref="user", cascade="all, delete, delete-orphan"
+        )
     else:
         email = ""
         password = ""
