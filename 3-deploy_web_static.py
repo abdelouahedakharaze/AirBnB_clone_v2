0fb89889e4b6516103311421e627912a1fe9c7c1
abdelouahed akharaze <abdelouahedpro1@gmail.com>
2024-05-17 15:11:08 +0100

    f

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
+env.hosts = ['54.236.45.92', '100.26.226.174']
 
 
 @runs_once
